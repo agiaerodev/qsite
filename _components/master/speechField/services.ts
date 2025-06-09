2ec5a0722610d947ec70789aab78b8a7ce6a4b5f
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2025-06-09 18:33:44 -0500

    Fix API request structure in getResponseAI method

--- _components/master/speechField/services.ts
+++ _components/master/speechField/services.ts
@@ -3,7 +3,7 @@ import baseService from 'modules/qcrud/_services/baseService'
 export default {
   async getResponseAI(api, prompt): Promise<any> {
     try {
-      const { data } = await baseService.create(api, { prompt })
+      const { data } = await baseService.create(api, prompt)
       return data
     } catch (error) {
       console.error(error)
