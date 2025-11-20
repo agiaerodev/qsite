a513fc8ad086d8e54d2c274304a8ba8d5168233f
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-11-20 15:25:04 -0500

    + added dynamickanban

---
+++ _components/master/dynamicKanban/index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <infomation
+      ref="modalInfortion"
+      @kanbanRefresh="kanbanRefresh"
+    />
+    <crud
+      :crudData="crudData"
+      :custom-data="customData"
+      :title="title"
+      ref="crudRequests"
+    />
+  </div>
+</template>
+<script>
+//Components
+
+import infomation from 'modules/qrequestable/_components/modals/information/components/index.vue'
+export default {
+  components: { infomation },
+  props: {
+    crudData: {
+      type: Object,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: false,
+      default: '',
+    },    
+  },
+  data() {
+    return {
+      showAsKanban: true
+    };
+  },
+  provide() {
+    return {
+      saveForm: this.saveForm,
+    };
+  },
+  computed: {
+    deletePermissions() {
+      return this.$hasAccess('requestable.requestables.destroy');
+    },
+    customData() {
+      return {
+        extraActions: [
+            {
+              label: this.showAsKanban ? 'Cambiar a modo tabla' : 'Camibiar a modo kanban',
+              props: {                
+                icon: this.showAsKanban ? 'fa-light fa-list' : 'fa-light fa-columns-3',
+                id: 'switch-button-crud'
+            },
+              action: () => this.switchMode()
+            }
+          ],
+
+        read: {
+          showAs: this.showAsKanban ? 'kanban' : 'table',          
+          actions: [
+            {
+              name: "viewEntity",
+              icon: "fa-light fa-eye",
+              label: this.$tr("isite.cms.label.show"),
+              format: (field) => {
+                return field.requestableUrl ? {} : { vIf: false };
+              },
+              action: (item) => {
+                if (item.requestableUrl)
+                  this.$helper.openExternalURL(item.requestableUrl);
+              },
+            },
+            {
+              name: "viewLead",
+              icon: "fa-light fa-info-circle",
+              color: "info",
+              tooltip: this.$tr("isite.cms.label.information"),
+              action: (item) => {
+                this.showModal(item)
+              },
+            },
+            {
+              name: "deleteLead",
+              label: this.$tr('isite.cms.label.delete'),
+              color: 'red',
+              icon: 'fa-light fa-trash-alt',
+              format: (field) => {
+                return { vIf: this.deletePermissions };
+              },
+              action: (item, automation) => {
+                this.delete(item, automation)
+              },
+            }
+          ],
+        },
+      };
+    },
+  },
+  methods: {
+    //update
+    async showModal(requestData) {
+      await this.$refs.modalInfortion.showRequestData(requestData);
+    },
+    //delete
+    async delete(item, automation = false) {
+      if (this.$refs.crudRequests.$refs.crudIndex.$refs.kanban) {
+        this.$refs.crudRequests.$refs.crudIndex.$refs.kanban.deleteKanbanCard(item, automation);
+      }
+    },
+    async kanbanRefresh(statusId) {
+      if (this.$refs.crudRequests.$refs.crudIndex.$refs.kanban) {
+          this.$refs.crudRequests.$refs.crudIndex.$refs.kanban.addCard(statusId);
+      }
+    },
+    async switchMode(){
+      this.showAsKanban = !this.showAsKanban
+      this.$refs.crudRequests.$refs.crudIndex.loadComponent().then(() => {
+        this.$refs.crudRequests.$refs.crudIndex.init()
+        this.$refs.crudRequests.$refs.crudIndex.getDataTable()
+      } ) ;
+    }
+  },
+};
+</script>
