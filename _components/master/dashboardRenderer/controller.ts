9050bc9ddc2b1939092c02b82a2c5e1b8cd2bfd8
Juan Escalante <jcec007@gmail.com>
2025-12-30 13:37:42 -0500

    New multi dashboard v1

--- _components/master/dashboardRenderer/controller.ts
+++ _components/master/dashboardRenderer/controller.ts
@@ -1,10 +1,10 @@
-import { 
-  computed, 
-  reactive, 
-  ref, 
-  onMounted, 
-  toRefs, 
-  defineAsyncComponent, 
+import {
+  computed,
+  reactive,
+  ref,
+  onMounted,
+  toRefs,
+  defineAsyncComponent,
   markRaw,
   watch,
 } from 'vue';
@@ -22,48 +22,61 @@ export default function controller(props: any, emit: any) {
   }
 
   const { dynamicFilterValues } = toRefs(props)
-  
+
   const state = reactive<{ views: View[] }>({
     views: [],
   })
 
   const computeds = {
-    filters: computed(() => ({ 
-      ...props.baseFilters, 
+    filters: computed(() => ({
+      ...props.baseFilters,
       ...props.dynamicFilterValues
     }))
   }
 
   const methods = {
     getDashboardElements: async (settings: Setting[]): Promise<View[] | []> => {
-      if (!settings) return []
-      const { hasAccess } = storeUtil
+      if (!settings) return [];
+      const { hasAccess } = storeUtil;
 
-      return Promise.all(settings.flatMap(quickCard => {
-        const { type, permission } = quickCard
-        if (!type) return []
-        if (permission && !hasAccess(permission)) return []
-        return {
-          component: markRaw(defineAsyncComponent(() => import(`./views/${type}`))),
-          ...quickCard
-        }
-      }))
+      return Promise.all(
+        settings.flatMap((quickCard) => {
+          const { type, permission } = quickCard;
+          if (!type) return [];
+          if (permission && !hasAccess(permission)) return [];
+          return {
+            component: markRaw(
+              defineAsyncComponent(() => import(`./views/${type}`))
+            ),
+            ...quickCard,
+          };
+        })
+      );
     },
-  }
+  };
 
   onMounted(async () => {
-    const { module, entity } = helper.getInfoFromPermission(route?.meta?.permission) || {}
-    store.globalFilters = computeds.filters.value
-    if (props.configName) {
-      refs.settings.value = await service.getConfig(props.configName)
-    } else {
-      const configName = `${module}.config.quickCards.${entity}`
-      refs.settings.value = await service.getConfig(configName)
-    }
+    const { module, entity } =
+      helper.getInfoFromPermission(route?.meta?.permission) || {};
+    store.globalFilters = computeds.filters.value;
 
-    const quickCards = await methods.getDashboardElements(refs.settings.value)
-    state.views = quickCards
-  })
+    if (!props.quickCards) {
+      if (props.configName) {
+        refs.settings.value = await service.getConfig(props.configName, true);
+      } else {
+        const configName = `${module}.config.quickCards.${entity}`;
+        refs.settings.value = await service.getConfig(configName);
+      }
+
+      const quickCards = await methods.getDashboardElements(
+        refs.settings.value
+      );
+      state.views = quickCards;
+    } else {
+      const quickCards = await methods.getDashboardElements(props.quickCards);
+      state.views = quickCards;
+    }
+  });
 
   watch(dynamicFilterValues, () => {
     store.globalFilters = computeds.filters.value
