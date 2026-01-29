f93fb1b0c3e00c3fbece0f255d09a80673a2b968
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2026-01-29 18:04:56 -0500

    + big cleanup , removed all unused components

--- _components/master/kanban/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const TYPE_STORE_KANBAN = 'kanbanStore()';
-export const TYPE_STORE_AUTOMATION_RULES = 'automationRulesStore()';
-export const startDateFormat = 'YYYY-MM-DD 00:00:00';
-export const endDateFormat = 'YYYY-MM-DD 23:59:59';
